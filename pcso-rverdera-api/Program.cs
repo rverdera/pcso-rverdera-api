938b8bba4534b33076190b33b42803eb556e7fa3
Richard Verdera, Jr <rverdera@pcso.gov.ph>
2022-05-23 15:20:35 +0800

    Implemented Delete Product API

--- pcso-rverdera-api/Program.cs
+++ pcso-rverdera-api/Program.cs
@@ -8,8 +8,19 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 
-app.MapGet("Hello", () => "This is very cool I hope");
+app.MapGet("Hello", () => "This is very cool I hope asdf");
 app.MapGet("Product", () => "Get some Product from azure SQL");
 
+app.MapPost("Product", (Product p) => "Product saved in SQL");
+app.MapDelete("Product", (string name) => "Deleted");
+
+app.MapPost("Chat", (Message msg) => "Message posted to chat SQL");
+
+
 app.Run();
+
+// Some Data Contracts
+
+public record class Message(string name, string Msg);
+public record class Product(string name, string Picture,int price);
 
